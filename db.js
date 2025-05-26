5ac1aab704860ff281720b8474126d86ed2e57d9
basanth1 <pbasanthkumar60@gmail.com>
2025-05-26 20:22:43 +0530

    Added my project112

--- db.js
+++ db.js
@@ -2,11 +2,13 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
+const { DATABASE_URL } = process.env;
+
 export const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "keeper",
-  password: "basanth123",
-  port: 5432,
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Important for Render's SSL
+  },
 });
+
 db.connect();
