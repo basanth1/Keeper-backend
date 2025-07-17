8377a1333fdd637f3a93e789864e16eeb216e45b
basanth1 <pbasanthkumar60@gmail.com>
2025-07-17 19:54:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const DATABASE_URL = 'postgresql://keeper_db_nni7_user:wBwhhsLVcM1wnxItq480jGetv8Q7aELv@dpg-d1sg8fgdl3ps73a9ba3g-a.oregon-postgres.render.com/keeper_db_nni7';
+const DATABASE_URL = 'postgresql://keeper_db_nni7_user:wBwhhsLVcM1wnxItq480jGetv8Q7aELv@dpg-d1sg8fgdl3ps73a9ba3g-a/keeper_db_nni7';
 
 
 export const db = new pg.Client({
